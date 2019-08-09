a0ad09817879ee0a1432ee4461d95257ed869fd4
Kaitlin Palmer <kaitlin.palmer@sdsu.edu>
2019-08-09 12:58:31 -0700

    update for structured version

--- Scripts/runSensitivtyLp.m
+++ Scripts/runSensitivtyLp.m
@@ -1,6 +1,6 @@
 % Function for returning the sensitiviery loop
 
-function [ExpScoresMeth nAgents] = runSensitivtyLp(examp,TimeThresh,SimThresh)
+function [ExpScoresMeth, nAgents] = runSensitivtyLp(simStruct,TimeThresh,SimThresh)
 % Returns mxn matrix of adjusted rand scores where m is the length of the
 % time threshold and n is the length of the similarity threshold
 % Input - simulation object, TimeThresh, SimThres
@@ -15,17 +15,17 @@ if nargin ==2
     nAgents =ExpScoresMeth;
     
     for jj = 1:length(SimThresh)
-        examp.Cluster_id =[];
-        examp.cutoff = SimThresh(jj);
+        simStruct.Cluster_id =[];
+        simStruct.cutoff = SimThresh(jj);
         
         for ii = 1:length(TimeThresh)
             
-            examp.Cluster_id =[];
-            examp.maxEltTime=(TimeThresh(ii));
-            examp.toaOnlyCluster();
-            examp.getRand();
-            ExpScoresMeth(ii,1) = examp.AdjRand;
-            nAgents(ii,1) = length(unique(examp.Cluster_id));
+            simStruct.Cluster_id =[];
+            simStruct.maxEltTime=(TimeThresh(ii));
+            simStruct.toaOnlyCluster();
+            simStruct.getRand();
+            ExpScoresMeth(ii,1) = simStruct.AdjRand;
+            nAgents(ii,1) = length(unique(simStruct.Cluster_id));
             
         end
         
@@ -38,17 +38,21 @@ else % Else there were three variables, so leave well enough alone
     idx =0;
     totit = length(SimThresh)*length(TimeThresh);
     for jj = 1:length(SimThresh)
-        examp.Cluster_id =[];
-        examp.cutoff = SimThresh(jj);
+        simStruct.Cluster_id =[];
+        simStruct.cutoff = SimThresh(jj);
         
         for ii = 1:length(TimeThresh)
             
-            examp.Cluster_id =[];
-            examp.maxEltTime=(TimeThresh(ii));
-            examp.updateChains;
-            examp.getRand();
-            ExpScoresMeth(ii,jj) = examp.AdjRand;
-            nAgents(ii,jj) = length(unique(examp.Cluster_id));
+            simStruct.Cluster_id =[];
+            simStruct.maxEltTime=(TimeThresh(ii));
+            
+            simStruct.chains =updateChainsEncounterFirst(simStruct);
+
+            
+            simStruct.Cluster_id= updateClusterID(simStruct);
+            
+            ExpScoresMeth(ii,jj) =  getRand(simStruct);
+            nAgents(ii,jj) = length(unique(simStruct.Cluster_id));
             
             %disp([num2str(idx) ' of ' num2str(totit) 'time/simthresh'])
             idx=idx+1;
