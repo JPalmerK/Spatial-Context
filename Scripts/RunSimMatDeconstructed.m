2ac4fe77f506ed98371aea3afd2966a5519cc797
Kaitlin Palmer <kaitlin.palmer@sdsu.edu>
2019-08-09 12:58:11 -0700

    Add full bit for sensitivity run

--- Scripts/RunSimMatDeconstructed.m
+++ Scripts/RunSimMatDeconstructed.m
@@ -33,7 +33,7 @@ child_idx = [1 2 3];
 
 
 
-[spaceWhale] =   createRandomSpaceWhale(0.75, 9, hyd_arr,...
+[spaceWhale] =   createRandomSpaceWhale(0.75, 6, hyd_arr,...
     array_struct,hydrophone_struct, ssp, grid_depth,...
     [array_struct.master, array_struct.slave(child_idx)]);
 
@@ -52,13 +52,91 @@ simStruct.maxEltTime =60;
 simStruct.cutoff = .25;
 simStruct.child_idx= child_idx;
 simStruct.randomMiss=0;
+simStruct.c =1500;
 simStruct.arrivalTable = UpdateArrTable(simStruct);
 simStruct.arrivalArray= UpdateArrArray(simStruct);
 simStruct.TDOA_vals = UpdateTDOA(simStruct);
-simStruct.Sim_mat= simMatIdealXcorrDist(simStruct);
+simStruct.Sim_mat= gather(simMatIdealXcorrDist(simStruct));
 simStruct.chains =updateChainsEncounterFirst(simStruct);
-simStruct.Cluster_id= updateClusterID(simStruct)
+simStruct.Cluster_id= updateClusterID(simStruct);
 
-AdjRand= getRand(simStruct)
 
+
+
+%% Sensitivity Experiment
+TimeThresh=linspace(10, 10*60,10);
+SimThresh = linspace(0,2.5,10);
+
+ExpScoresMeth_out2D = [];
+ExpScoresMeth_outTDOA = [];
+
+allSimStructs =[];
+
+% Make copy of simulation structures
+
+    simStruct1 = simStruct;
+    simStruct2= simStruct;
+
+tic
+for ii=1:1
+    % Replace the space whale component
+    [spaceWhale] =   createRandomSpaceWhale(0.75, 4, hyd_arr,...
+        array_struct,hydrophone_struct, ssp, grid_depth,...
+        [array_struct.master, array_struct.slave(child_idx)]);
+    
+    % Copy for each of the methods
+    simStructNew = simStruct;
+
+    
+    % Update the arrival array and simulation matrix
+    simStructNew.spaceWhale=spaceWhale;
+    simStructNew.arrivalTable = UpdateArrTable(simStructNew);
+    simStructNew.arrivalArray= UpdateArrArray(simStructNew);
+    simStructNew.TDOA_vals = UpdateTDOA(simStructNew);
+    
+    
+    % Create copy for TDOA method
+    simStructTDOA = simStructNew;
+    
+    
+     % TDOA only method
+     simStructTDOA.Sim_mat = simMatTDOAonly(simStructTDOA);
+     
+      %Run the sensitivity loop
+      [ExpScoresMethTDOA nAgents] = runSensitivtyLp(simStructTDOA,TimeThresh,SimThresh);
+      ExpScoresMeth_outTDOA =[ExpScoresMeth_outTDOA, ExpScoresMethTDOA]
+     
+    
+    
+    
+    % 2D XCORR METHOD
+    simStructNew.Sim_mat =simMatIdealXcorrDist(simStructNew);
+    
+    % Run the sensitivity loop
+    [ExpScoresMeth2D nAgents] = runSensitivtyLp(simStructNew,TimeThresh,SimThresh);
+     ExpScoresMeth_out2D = [ExpScoresMeth_out2D,ExpScoresMeth2D];
+     
+     
+     
+end
+toc
+
+
+%% Classifier Performance Experiment
+aa =14
+betaParm1= [aa aa aa];
+betaParm2=[aa-1 aa-3 aa-6];
+simStruct.betaParm1 = betaParm1(1);
+simStruct.betaParm2=betaParm2(1);
+perf = estClassifierPerf(simStruct);
+
+%% Clock Drift Experiment
+simStruct.c = 1500;
+simStruct.drift=1.5;
+simStruct.assSec=2;
+simStruct.hydrophone_struct = hydrophone_struct;
+simStruct.arrivalArray= UpdateArrArray(simStruct);
+simStruct.betaParm1 = betaParm1(1);
+simStruct.betaParm2=betaParm2(1);
+perf = estClassifierPerf(simStruct);
 
