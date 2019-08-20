47501d4669e775937c8dfde583ef5907f7aedb8a
Kaitlin Palmer <kaitlin.palmer@sdsu.edu>
2019-08-20 09:33:04 -0700

    remove some hard coding

--- Scripts/RunSimMatDeconstructed.m
+++ Scripts/RunSimMatDeconstructed.m
@@ -41,7 +41,7 @@ simStruct=struct();
 simStruct.spaceWhale=spaceWhale;
 simStruct.array_struct=array_struct;
 simStruct.truncateKm=10;
-simStruct.s = 8
+simStruct.s = 8;
 simStruct.PosUncertsigma = 0.0004^2 +.1^2 + .3^2;
 % Position/soundspeed uncertainty, see Eva's paper
 % receiver position = 5 m -> 5/1500 = 0.0004 sec
@@ -69,62 +69,75 @@ simStruct.TDOA_vals = UpdateTDOA(simStruct);
 % elapsed time are trimmed such that it's ok to calculate the sensitivy
 % threshold when the encounter is too large, but will break when it's too
 % small
-TimeThresh=fliplr(linspace(1, 30,20)); 
+nIters =200;
+TimeThresh=fliplr(linspace(1, 120, 50));
 SimThresh = linspace(0,1,30);
 SimThresh1 =linspace(0,1,30);
 
-ExpScoresMeth_out2D = zeros(length(TimeThresh), length(SimThresh), 20)/0;
+ExpScoresMeth_out2D = zeros(length(TimeThresh), length(SimThresh), nIters)/0;
 ExpScoresMeth_outTDOA = ExpScoresMeth_out2D;
 ExpScoresMeth_outMaxProd = ExpScoresMeth_out2D;
 
 allSimStructs =[];
 
+% Number of agents (experiemnts)
+nAgents = [3,6,9];
+
+% Structure for output
+AgentExp = struct();
+
 
 tic
 
-parfor ii=1:5
-    % Replace the space whale component
-    [spaceWhale] =   createRandomSpaceWhale(1, 6, hyd_arr,...
-        array_struct,hydrophone_struct, ssp, grid_depth,...
-        [array_struct.master, array_struct.slave(child_idx)]);
+for jj =1:length(nAgents)
+    nAgent = nAgents(jj);
+    parfor ii=1:nIters
+        % Replace the space whale component
+        [spaceWhale] =   createRandomSpaceWhale(1, nAgent, hyd_arr,...
+            array_struct,hydrophone_struct, ssp, grid_depth,...
+            [array_struct.master, array_struct.slave(child_idx)]);
+        
+        % Copy for each of the methods
+        simStructNew = simStruct;
+        
+        
+        % Update the arrival array and simulation matrix
+        simStructNew.spaceWhale=spaceWhale;
+        simStructNew.arrivalTable = UpdateArrTable(simStructNew);
+        simStructNew.arrivalArray= UpdateArrArray(simStructNew);
+        simStructNew.TDOA_vals = UpdateTDOA(simStructNew);
+        
+        
+        % Create copy for TDOA method
+        simStructTDOA = simStructNew;
+        
+        % TDOA only method
+        simStructTDOA.Sim_mat = simMatTDOAonly(simStructTDOA);
+        
+        %Run the sensitivity loop
+        [ExpScoresMethTDOA, ~] = runSensitivtyLp(simStructTDOA,TimeThresh,SimThresh);
+        ExpScoresMeth_outTDOA(:,:,ii) = ExpScoresMethTDOA;
+        %
+        % Max of mean
+        simStructMaxMean = simStructNew;
+        
+        
+        % Product Method
+        simStructMaxMean.Sim_mat = simMatMaxofProd(simStructMaxMean);
+        
+        
+        %Run the sensitivity loop
+        [ExpScoresMethMaxMean, ~] = runSensitivtyLp(simStructMaxMean,TimeThresh,SimThresh);
+        ExpScoresMeth_outMaxProd(:,:,ii) = ExpScoresMethMaxMean;
+        
+        ii
+        
+    end
+    AgentExp(jj).TDOA =ExpScoresMeth_outTDOA;
+    AgentExp(jj).MaxProd = ExpScoresMeth_outMaxProd;
     
-    % Copy for each of the methods
-    simStructNew = simStruct;
     
     
-    % Update the arrival array and simulation matrix
-    simStructNew.spaceWhale=spaceWhale;
-    simStructNew.arrivalTable = UpdateArrTable(simStructNew);
-    simStructNew.arrivalArray= UpdateArrArray(simStructNew);
-    simStructNew.TDOA_vals = UpdateTDOA(simStructNew);
-    
-    
-    % Create copy for TDOA method
-    simStructTDOA = simStructNew;
-    
-    % TDOA only method
-    simStructTDOA.Sim_mat = simMatTDOAonly(simStructTDOA);
-    
-    %Run the sensitivity loop
-    [ExpScoresMethTDOA, ~] = runSensitivtyLp(simStructTDOA,TimeThresh,SimThresh);
-    ExpScoresMeth_outTDOA(:,:,ii) = ExpScoresMethTDOA;
-    
-      % Max of mean 
-    simStructMaxMean = simStructNew;
-    
-    % Mean method
-    %simStructMaxMean.Sim_mat = simMatMaxofMean(simStructMaxMean);
-    
-    % Product Method
-    simStructMaxMean.Sim_mat = simMatMaxofProd(simStructMaxMean)
-    
-    
-    %Run the sensitivity loop
-    [ExpScoresMethMaxMean, ~] = runSensitivtyLp(simStructMaxMean,TimeThresh,SimThresh);
-    ExpScoresMeth_outMaxProd(:,:,ii) = ExpScoresMethMaxMean;
-    
-    ii
-   
 end
 toc
 
@@ -133,27 +146,27 @@ figure(1)
 % subplot(3,1,1)
 % imagesc(SimThresh1nanmedian(ExpScoresMeth_out2D,3)) ,  axis xy, colorbar
 % title('Median ARI Max of Mean')
-% 
-% 
+%
+%
 subplot(2,1,1)
-imagesc(TimeThresh,SimThresh, nanmedian(ExpScoresMeth_outTDOA,3)) ,  axis xy, colorbar
+imagesc(TimeThresh,SimThresh, nanmedian(ExpScoresMeth_outTDOA,3)) ,  axis xy  colorbar
 title('TDOA only')
 
 
 
 subplot(2,1,2)
-imagesc(TimeThresh,SimThresh1, nanmean(ExpScoresMeth_outMaxProd,3)) ,  axis xy, colorbar
+imagesc(TimeThresh,SimThresh1, nanmean(ExpScoresMeth_outMaxProd,3)) ,  axis xy  colorbar
 title('Spatial Method')
 xlabel('Time Threshold (s)')
 ylabel('Similarity Trhreshold')
 
 %% Classifier Performance Experiment
-aa =14
+aa =14;
 betaParm1= aa;
 betaParm2=[aa-2 aa-5 aa-7];
 
 perf_out = [];
-nruns =100;
+nruns =500;
 
 timethresh = 10:10:60;
 
@@ -162,31 +175,31 @@ parfor ii=1:length(betaParm2)
     beta2 = betaParm2(ii);
     
     for jj =1:nruns
-    % Replace the space whale component
-    [spaceWhale] =   createRandomSpaceWhale(1, 6, hyd_arr,...
-        array_struct,hydrophone_struct, ssp, grid_depth,...
-        [array_struct.master, array_struct.slave(child_idx)]);
-    
-    % Copy for each of the methods
-    simStructNew = simStruct;
-    simStructNew.spaceWhale = spaceWhale;
-    simStructNew.arrivalTable = UpdateArrTable(simStructNew);
-    simStructNew.arrivalArray= UpdateArrArray(simStructNew);
-    simStructNew.TDOA_vals = UpdateTDOA(simStructNew);
-    simStructNew.betaParm1 = betaParm1;
-    simStructNew.betaParm2 = beta2;
-    simStructNew.cutoff =.85;
-    simStructNew.maxEltTime = 20;
-    
-    % Max of mean
-    simStructNew.Sim_mat = simMatMaxofProd(simStructNew);
-    simStructNew.chains =updateChainsEncounterFirst(simStructNew);
-    simStructNew.Cluster_id= updateClusterID(simStructNew);
-    perf = estClassifierPerf(simStructNew);
-    
-    perf_row(jj).perf = perf;
-    
-    
+        % Replace the space whale component
+        [spaceWhale] =   createRandomSpaceWhale(1, 6, hyd_arr,...
+            array_struct,hydrophone_struct, ssp, grid_depth,...
+            [array_struct.master, array_struct.slave(child_idx)]);
+        
+        % Copy for each of the methods
+        simStructNew = simStruct;
+        simStructNew.spaceWhale = spaceWhale;
+        simStructNew.arrivalTable = UpdateArrTable(simStructNew);
+        simStructNew.arrivalArray= UpdateArrArray(simStructNew);
+        simStructNew.TDOA_vals = UpdateTDOA(simStructNew);
+        simStructNew.betaParm1 = betaParm1;
+        simStructNew.betaParm2 = beta2;
+        simStructNew.cutoff =.85;
+        simStructNew.maxEltTime = 20;
+        
+        % Max of mean
+        simStructNew.Sim_mat = simMatMaxofProd(simStructNew);
+        simStructNew.chains =updateChainsEncounterFirst(simStructNew);
+        simStructNew.Cluster_id= updateClusterID(simStructNew);
+        perf = estClassifierPerf(simStructNew);
+        
+        perf_row(jj).perf = perf;
+        
+        
     end
     perf_out=[perf_out;perf_row];
     
@@ -213,7 +226,7 @@ for jj=1:length(betaParm2)
     xlabel(['Change in Error Rate: Initial Error ', num2str(round(...
         betacdf(.5, betaParm1, betaParm2(jj)),2))])
     xlim([-.8 .8])
-     
+    
     (sum(perf>0)/length(perf))-(sum(perf<0)/length(perf))
     
 end
@@ -235,7 +248,7 @@ association_sec = [0 4 6];
 perf_assocExp =struct();
 nRuns = 10;
 for ii=1:length(association_sec)
-  
+    
     assoc = association_sec(ii);
     perf_driftExp =[];
     for jj = 1:length(clock_drift)
@@ -271,7 +284,7 @@ for ii=1:length(association_sec)
         
     end
     
-      perf_assocExp(ii).AssocExp = perf_driftExp;
+    perf_assocExp(ii).AssocExp = perf_driftExp;
 end
 
 
